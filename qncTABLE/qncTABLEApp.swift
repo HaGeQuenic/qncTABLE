f9589ff6b28bf4fefc82b5dacfea002bd1f71d03
Harald <hage@quenic.com>
2026-08-29 14:59:41 +0200

    Initial Commit

---
+++ qncTABLE/qncTABLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  qncTABLEApp.swift
+//  qncTABLE
+//
+//  Created by Harald on 29.08.26.
+//
+
+import SwiftUI
+
+@main
+struct qncTABLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
